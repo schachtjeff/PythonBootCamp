54f28a44624deca664ce422f39ab4fe5130cddc0
schachtjeff <schachtj46@gmail.com>
2025-03-04 13:02:01 -0800

    update the etch-a-sketch

--- Day19/main.py
+++ Day19/main.py
@@ -7,6 +7,25 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
+def move_backwards():
+    tim.backward(10)
+
+def move_counterClockwise():
+    tim.left(15)
+
+def move_clockwise():
+    tim.right(15)
+
+def clear_drawing():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_counterClockwise)
+screen.onkey(key="d", fun=move_clockwise)
+screen.onkey(key="c", fun=clear_drawing)
 screen.exitonclick()
