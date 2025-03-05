cb658b783ea00b143e3045875959a667fb364753
schachtjeff <schachtj46@gmail.com>
2025-03-05 15:12:07 -0800

    add the game over

--- snake_game/main.py
+++ snake_game/main.py
@@ -4,6 +4,9 @@ from food import Food
 from scoreboard import Scoreboard
 import time
 
+WALLS = 280
+NEG_WALLS = -280
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -31,5 +34,14 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
+
+    # Detect collision with the wall
+    if snake.head.xcor() > WALLS \
+            or snake.head.xcor() < NEG_WALLS \
+            or snake.head.ycor() > WALLS \
+            or snake.head.ycor() < NEG_WALLS:
+        game_is_on = False
+        scoreboard.game_over()
+
 
 screen.exitonclick()
