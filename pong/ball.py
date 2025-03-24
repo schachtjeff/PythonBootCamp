d573d35d261f22a2ee3a7557c27b6333c2c6e432
schachtjeff <schachtj46@gmail.com>
2025-03-24 16:36:42 -0700

    make the out

--- pong/ball.py
+++ pong/ball.py
@@ -20,3 +20,7 @@ class Ball(Turtle):
 
     def x_bounce(self):
         self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0,0)
+        self.x_bounce()
--- pong/main.py
+++ pong/main.py
@@ -36,4 +36,12 @@ while game_is_on:
     if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
         ball.x_bounce()
 
+    # Detect when R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect when L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 screen.exitonclick()
