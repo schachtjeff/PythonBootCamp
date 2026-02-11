5bcc42688d57d957107a1f9658ea2f16dd4d84d8
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-02-10 22:41:21 -0800

    storing variables

---
+++ Beginner/1_variables/variables.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+# print name value from input
+print(name)
+
+#challenge!
+# Get number of characters in string dynamically
+char_num = len(name)
+print("Length in string: " + str(char_num))
