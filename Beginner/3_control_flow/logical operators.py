42c6cfb440983000b4494a6948b509cc76b658f8
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-02-11 18:07:20 -0800

    logical operators

---
+++ Beginner/3_control_flow/logical operators.py
@@ -0,0 +1,28 @@
+# incorporate aged 45-55 is $0
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5")
+    elif age > 18:
+        if age >= 45 and age <= 55:
+            bill = 0
+            print("Your ticket is FREE!")
+        else:
+            bill = 12
+            print("Adult tickets are $12")
+    else:
+        bill = 7
+        print("Youth tickets are $7")
+    
+    answer = input("Do you want to have a photo taken? Type y for Yes and n for No.")
+    if answer == "y":
+        bill += 3
+    print(f"Your bill is: {bill}")
+else:
+    print("Sorry!  You are not allowed.")
