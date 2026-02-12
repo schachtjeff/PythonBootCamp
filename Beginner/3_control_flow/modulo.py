ef89c71fc9584e6724059ab64f43e5a98ade433e
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-02-11 16:06:49 -0800

    modulo

---
+++ Beginner/3_control_flow/modulo.py
@@ -0,0 +1,9 @@
+# Even number divided by 2 is clean.  12 % 2 = 0
+check = int(input("What is the number to check: "))
+
+#print(check % 2)
+
+if check % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
