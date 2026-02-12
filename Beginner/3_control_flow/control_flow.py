8bc6babe53e8a6210dfee8dd13dd5dc1b17fe4f8
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-02-11 16:00:40 -0800

    control flow

---
+++ Beginner/3_control_flow/control_flow.py
@@ -0,0 +1,14 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry!  You are not allowed.")
+
+# >: greater than
+# <: less than
+# <= less than or equal to
+# <= greater than or equal to
+# != not equal
+# == equal to
