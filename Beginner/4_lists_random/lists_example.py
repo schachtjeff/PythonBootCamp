459000a3debe47aa229597b4b3ffeec449a0ac5d
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-02-13 14:09:12 -0800

    using lists

---
+++ Beginner/4_lists_random/lists_example.py
@@ -0,0 +1,10 @@
+#Example lists
+states_of_america = ["Deleware", "Pennsylvania"]
+
+print(states_of_america[0])
+
+# adding to the list
+states_of_america.append("Jeffreyland")
+
+# extending is adding one or more to the list.
+states_of_america.extend("Jeffreyland2", "stuff-o-place")
