88928c9d775062e82660ff0ed1b965dfb1cd78d0
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-02-24 15:15:27 -0800

    weeks left excercise

---
+++ Beginner/8_func_params/weeks_left_excercise.py
@@ -0,0 +1,6 @@
+def life_in_weeks(current_age):
+    difference_age = 90 - current_age
+    weeks_left = 52 * difference_age
+    print(f"You have {weeks_left} weeks left.")
+    
+life_in_weeks(56)
