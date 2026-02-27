249d06b479f71ab8c43d3f553c9d63cc5155365e
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-02-27 08:29:46 -0800

    ascii art and flowchart

---
+++ Beginner/11_capstone_blackjack/ascii_art.py
@@ -0,0 +1,17 @@
+# ASCII Art of Cards from -> https://ascii.co.uk/art/cards
+# Created by: ejm98
+
+blackjack_display = r'''
+@@@@  <3  <>  {> -%   @@@@
+      
+      Blackjack Game!
+     _____  
+    |J  ww|  _____
+    | ^ {)| |A .  |
+    |(.)% | | /.\ |
+    | | % | |(_._)|
+    |__%%[| |  |  |
+            |____V|
+    
+@@@@  <3  <>  {> -%   @@@@
+'''
