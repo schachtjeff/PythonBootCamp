f6f53a08c552f98d653ab48ac2ecaf52ee4123d2
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-03-02 09:23:36 -0800

    debug fizzbuzz

---
+++ Beginner/13_debugging/fizzBuzz.py
@@ -0,0 +1,37 @@
+#Debug FizzBuzz
+'''
+- Read the code in exercise.py
+
+- Spot the problems 🐞. 
+
+- Modify the code to fix the program. 
+
+- No shortcuts 
+
+The code needs to print the solution to the FizzBuzz game.   
+
+- Your program should print each number from 1 to x where x is the input number. 
+
+- However when the number is divisible by 3 then instead of printing the number it should print "Fizz".   
+
+- When the number is divisible by 5, then instead of printing the number it should print "Buzz". 
+
+- And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz". 
+'''
+
+# Target is the number up to which we count
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        # Error in 'or' -> if number % 3 == 0 or number % 5 == 0:
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        #change if to elif
+        elif number % 3 == 0:
+            print("Fizz")
+        #change if to elif
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+            # not print with number in array.
+            #print([number])
