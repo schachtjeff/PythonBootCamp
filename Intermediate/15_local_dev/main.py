2130993ea2c732a4467b40c13612fd97bbb3025a
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-03-03 08:43:08 -0800

    global data entered

---
+++ Intermediate/15_local_dev/main.py
@@ -0,0 +1,36 @@
+# Coffee Machine Project
+# Project Requirments download can be found below:
+#  https://att-c.udemycdn.com/2020-11-10_10-05-10-450b82c80c408a663d35e3bd60526b24/original.pdf?response-content-disposition=attachment%3B+filename%3DCoffee%2BMachine%2BProgram%2BRequirements.pdf&Expires=1772569985&Signature=Whz4RkdCGChNRQmW~fq7XW48bCUC8Myq9mTd-Hg9gPd~owEx3ERa9QF3x-5ryxjYcB1CAoqd44rxWI8NBRKWIaaSREPYh1KORQ-YGG8YypkLK2mxOX~VeaqnIFDw6EK7n6tpdOkLabq-HUU~k4nW79YtAkRWJ3WdgzQwrBedjqQa8t0NpUW~5E~BY6MdmaQdcRzYf976~00IJoq~rza8vG-qoJf4WiOaYKA~Z0iylDpIMgCZDWb7ES~2~Un12AFBm2UcUVphNXwLKomrJrDX53ePA60a19xto774JoisKzNVPHRAd2l6NAoJvXd-BievC8wGI9lpa61RFRbAhOLA0A__&Key-Pair-Id=K3MG148K9RIRF4
+
+# Global
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+RESOURCES = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
