1489cbb5576274ffd00ee2903320a9f90ca6d570
schachtjeff <schachtj46@gmail.com>
2026-03-31 12:45:48 -0700

    Init drawing of Turtle

--- Intermediate/16_oop/main.py
+++ Intermediate/16_oop/main.py
@@ -1,9 +1,19 @@
 # imports
-from turtle import Turtle
-from turtle import Turtle
-import another_module
-print(another_module.another_variable)
+from turtle import Turtle, Screen
+#import another_module
+
+#print(another_module.another_variable)
 
 timmy = Turtle()
 print(timmy)
 
+#change the shape
+timmy.shape("turtle")
+timmy.color("blue")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
+# allow window to continue running until click
+my_screen.exitonclick()
