c4c93699a32a8dbea3ee1e6f307279f5c4400630
schachtjeff <schachtj46@gmail.com>
2026-03-31 14:00:58 -0700

    adding the coffee machine starting code

--- Intermediate/16_oop/main.py
+++ Intermediate/16_oop/main.py
@@ -4,16 +4,24 @@ from turtle import Turtle, Screen
 
 #print(another_module.another_variable)
 
-timmy = Turtle()
-print(timmy)
+#timmy = Turtle()
+#print(timmy)
 
 #change the shape
-timmy.shape("turtle")
-timmy.color("blue")
-timmy.forward(100)
+#timmy.shape("turtle")
+#timmy.color("blue")
+#timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
+#my_screen = Screen()
+#print(my_screen.canvheight)
 
 # allow window to continue running until click
-my_screen.exitonclick()
+#my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Chamander"])
+table.add_column("Type", ["Elcectric", "Water", "Fire"])
+table.align = "l"
+print(table)
