9a9254ec67fb30f9e681e2cfa6a9480727044b7d
schachtjeff <schachtj46@gmail.com>
2026-04-01 08:46:05 -0700

    init a class topic

--- Intermediate/17_oop_quiz/main.py
+++ Intermediate/17_oop_quiz/main.py
@@ -1,5 +1,20 @@
 # Creating a class
 class User:
-    pass
+    def __init__(self, user_id, username, followers = 0):
+        print("new user being created...")
+        self.id = user_id
+        self.username = username
+        self.followers = followers
 
-user_1 = User()
+user_1 = User(user_id="001", username="Jeff")
+# attributes attached to the object
+#user_1.id = "001"
+#user_1.username = "Jeff"
+
+print(user_1.username)
+
+user_2 = User(user_id="002", username="Steve")
+#user_2.id = "002"
+#user_2.username = "Steve"
+
+print(user_2.username)
