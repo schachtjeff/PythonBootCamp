a7a860f92ef07ccb7bba474685958d75c76f30a5
schachtjeff <schachtj46@gmail.com>
2026-04-15 14:27:09 -0700

    creating cars

--- Intermediate/23_tutle_cross/car_manager.py
+++ Intermediate/23_tutle_cross/car_manager.py
@@ -1,11 +1,39 @@
 #Imports
+import random
 from turtle import Turtle
 
 # Constants
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+START_X_POS = 300
+RANDOM_Y_POS_LOWER = -250
+RANDOM_Y_POS_UPPER = 250
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.all_moving_cars = []
+
+    def create_new_car(self) -> None:
+        if self.is_create_new_car():
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y_pos = random.randint(RANDOM_Y_POS_LOWER, RANDOM_Y_POS_UPPER)
+            new_car.goto(START_X_POS, random_y_pos)
+            self.all_moving_cars.append(new_car)
+
+    def move_cars(self) -> None:
+        for car in self.all_moving_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
+
+    def is_create_new_car(self) -> bool:
+        #Random chance every 1 and 6 chances
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            return True
+        else:
+            return False
+
--- Intermediate/23_tutle_cross/main.py
+++ Intermediate/23_tutle_cross/main.py
@@ -13,6 +13,7 @@ screen.tracer(0)
 
 # Create the turtle to cross
 timmy = Player()
+car_manager = CarManager()
 
 # Listen for up arrow key to move the player
 screen.listen()
@@ -22,6 +23,9 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_new_car()
+    car_manager.move_cars()
 
 # Exit the game when done.
 screen.exitonclick()
