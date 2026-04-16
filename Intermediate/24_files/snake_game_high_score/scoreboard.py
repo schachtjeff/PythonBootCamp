86ee4b9cb78a21f1963ae68d0d6a54c3ddb907e1
schachtjeff <schachtj46@gmail.com>
2026-04-16 08:58:27 -0700

    persist data in snake game

--- Intermediate/24_files/snake_game_high_score/scoreboard.py
+++ Intermediate/24_files/snake_game_high_score/scoreboard.py
@@ -10,7 +10,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.get_high_score()
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -29,6 +29,7 @@ class Scoreboard(Turtle):
     def reset_score(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.update_data()
         self.score = 0
         self.update_scoreboard()
 
@@ -37,3 +38,12 @@ class Scoreboard(Turtle):
         # clear is so score doesn't overlap previous scores
         #self.clear()
         self.update_scoreboard()
+
+    def get_high_score(self) -> int:
+        with open("data.txt", mode="r") as file:
+            return int(file.read())
+
+    def update_data(self) -> None:
+        with open("data.txt", mode="w") as file:
+            file.write(str(self.high_score))
+
