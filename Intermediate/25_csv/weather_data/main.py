ed2e550b53a5c3e2f137f01489f7eaea35d66973
schachtjeff <schachtj46@gmail.com>
2026-04-17 13:41:38 -0700

    work with pandas

--- Intermediate/25_csv/weather_data/main.py
+++ Intermediate/25_csv/weather_data/main.py
@@ -21,4 +21,44 @@ import csv
 import pandas
 
 data = pandas.read_csv('weather_data.csv')
-print(data["temp"])
+# should get a dataframe
+print(type(data))
+#print(data["temp"])
+series_list = data["temp"]
+print(type(series_list))
+
+# Create as a dictionary
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+num_temps = 0
+sum_temps = 0
+for temp in temp_list:
+    sum_temps += temp
+    num_temps += 1
+avg_temp = sum_temps / num_temps
+print(avg_temp)
+
+#or
+print(data["temp"].mean())
+print(data["temp"].min())
+print(data["temp"].max())
+
+# Also works, column is an attribute
+print(data.temp.mean())
+
+# print row
+print(data[data.day == "Monday"])
+
+# print highest temp row
+print(data[data.temp == data.temp.max()])
+
+#
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+# get the temp in Ferinheit
+print(monday.temp * (9/5) + 32)
