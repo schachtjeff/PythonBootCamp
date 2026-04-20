64319f2eff1086cc01756bd83b70d93b1e2042d4
schachtjeff <schachtj46@gmail.com>
2026-04-20 11:28:24 -0700

    tkinter items, args and kwargs

--- Intermediate/27_args_kwargs/tkinter_example.py
+++ Intermediate/27_args_kwargs/tkinter_example.py
@@ -9,6 +9,26 @@ window.minsize(width=500, height=300)
 my_label = tk.Label(window, text="My label", font=("Arial", 25, "bold"))
 my_label.pack()
 
+# Change the text in either of the following
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+#funciton to do with button
+def button_clicked() -> None:
+    print("Button clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+#Button
+button = tk.Button(text="Click me", command=button_clicked)
+button.pack()
+
+#Entry component
+input = tk.Entry(width=40)
+input.pack()
+print(input.get())
+
+
 
 # Keep window on screen and always at the end
 window.mainloop()
