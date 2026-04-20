1b3faad09da1bbf841b6c49da29891a00e7223b6
schachtjeff <schachtj46@gmail.com>
2026-04-19 19:43:04 -0700

    adding quiz

--- Intermediate/26_comprehension/dict_comprehension.py
+++ Intermediate/26_comprehension/dict_comprehension.py
@@ -8,3 +8,18 @@ print(student_scores)
 # create a dict using dict comprehension
 passed_students = {student:score for (student, score) in student_scores.items() if score >= 60}
 print(passed_students)
+
+# Count words including punctuation
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+words_list = sentence.split(' ')
+print(words_list)
+result = {word:len(word) for word in words_list}
+print(result)
+
+
+# Do the weather
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+
+weather_f = {day:(degrees_c*9/5)+32 for (day, degrees_c) in weather_c.items()}
+
+print(weather_f)
