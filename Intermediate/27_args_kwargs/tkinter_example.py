06b2090e635ff70c8f09821b7a3ddf6b4804f48a
schachtjeff <schachtj46@gmail.com>
2026-04-20 11:36:17 -0700

    other tkinter

--- Intermediate/27_args_kwargs/tkinter_example.py
+++ Intermediate/27_args_kwargs/tkinter_example.py
@@ -16,6 +16,7 @@ my_label.config(text="New Text")
 #funciton to do with button
 def button_clicked() -> None:
     print("Button clicked")
+    # Gets the input box and then puts text in label
     new_text = input.get()
     my_label.config(text=new_text)
 
