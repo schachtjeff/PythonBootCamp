279c6b399087e3878c5608a40d6f03387281baa6
schachtjeff <schachtj46@gmail.com>
2026-04-21 09:29:42 -0700

    timer and countdown mechanisms

--- Intermediate/28_dynamic_typing/main.py
+++ Intermediate/28_dynamic_typing/main.py
@@ -1,5 +1,7 @@
 # Imports
 import tkinter as tk
+import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,20 +14,33 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(5 * 60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        # Execute a command 'after' a time delay
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
 # Create the canvas with size, background color, and removing the outline
 canvas = tk.Canvas(window, width=200, height=230, bg=YELLOW, highlightthickness=0)
 tomato_img = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 115, image=tomato_img)
-canvas.create_text(100, 140, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 140, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
 # Add Timer label
@@ -33,7 +48,7 @@ timer_label = tk.Label(text="Timer", font=(FONT_NAME, 50), bg=YELLOW, fg=GREEN)
 timer_label.grid(row=0, column=1)
 
 # Add Start and Reset Buttons
-start_button = tk.Button(text="Start", highlightthickness=0)
+start_button = tk.Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(row=2, column=0)
 reset_button = tk.Button(text="Reset", highlightthickness=0)
 reset_button.grid(row=2, column=2)
@@ -41,7 +56,5 @@ reset_button.grid(row=2, column=2)
 # Add Counter Label
 counter_label = tk.Label(text="✔", font=(FONT_NAME, 20, "bold"), bg=YELLOW, fg=GREEN)
 counter_label.grid(row=3, column=1)
-
-
 
 window.mainloop()
