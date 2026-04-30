d68cbaea84fce3cff3ee592561d03402e7325656
schachtjeff <schachtj46@gmail.com>
2026-04-30 10:36:52 -0700

    add timer

--- Intermediate/31_flash_cards/main.py
+++ Intermediate/31_flash_cards/main.py
@@ -9,15 +9,25 @@ BACKGROUND_COLOR = "#B1DDC6"
 LANG_LABEL_FONT = ("Arial", 40, "italic")
 WORD_LABEL_FONT = ("Arial", 60, "bold")
 SPANISH_TO_ENGLISH_CSV = "data/es-en-words.csv"
+current_card = {}
 
 
-# ---------------------------- FLIP CARD ------------------------------- #
-def flip_card() -> None:
+# ---------------------------- Next CARD ------------------------------- #
+def next_card() -> None:
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     words_to_learn = read_csv_to_df()
     current_card = choice(words_to_learn)
-    canvas.itemconfig(card_language, text="Spanish")
-    canvas.itemconfig(card_word, text=current_card["Spanish"])
+    canvas.itemconfig(card_language, text="Spanish", fill="black")
+    canvas.itemconfig(card_word, text=current_card["Spanish"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_image)
+    flip_timer = window.after(3000, func=flip_card)
 
+# ---------------------------- Flip CARD ------------------------------- #
+def flip_card() -> None:
+    canvas.itemconfig(card_language, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_image)
 
 # ---------------------------- READ CSV ------------------------------- #
 def read_csv_to_df():
@@ -35,10 +45,14 @@ window = tk.Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+# Change the language after 3 seconds
+flip_timer = window.after(3000, func=flip_card)
+
 # Create Canvas, card with texts
 canvas = tk.Canvas(window, width=800, height=526)
 card_front_image = tk.PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_image)
+card_back_image = tk.PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_image)
 card_language = canvas.create_text(400, 150, text="Language", font=LANG_LABEL_FONT)
 card_word = canvas.create_text(400, 263, text="Word", font=WORD_LABEL_FONT)
 canvas.grid(row=0, column=0, columnspan=2)
@@ -48,15 +62,15 @@ card_button = tk.Button(image=card_front_image, highlightthickness=0)
 
 # Buttons
 cross_image = tk.PhotoImage(file="images/wrong.png")
-cross_button = tk.Button(image=cross_image, highlightthickness=0, command=flip_card)
+cross_button = tk.Button(image=cross_image, highlightthickness=0, command=next_card)
 cross_button.grid(row=1, column=0)
 
 correct_image = tk.PhotoImage(file="images/right.png")
-correct_button = tk.Button(image=correct_image, highlightthickness=0, command=flip_card)
+correct_button = tk.Button(image=correct_image, highlightthickness=0, command=next_card)
 correct_button.grid(row=1, column=1)
 
 # Generate the first card
-flip_card()
+next_card()
 
 # Keep window open until close/exit
 window.mainloop()
